fd16a404cfe39074d5eb7e0453a49be0ef0a33ce
Kady Tran <kadytrn@gmail.com>
2025-11-05 17:41:56 -0800

    added footer code inside components for project draft

---
+++ src/project-draft/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <footer>
+            <div class="footer-nav">
+                <a href="index.html">Home</a>
+                <a href="shop.html">Businesses</a>
+                <a href="proposal.html">About</a>
+        </div>
+        <p>&copy; 2025 ThriveTogether | Contact: info@thrivetogether.com</p>
+        </footer>
+    );
+}
