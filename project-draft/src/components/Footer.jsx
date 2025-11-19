4c5c36e57b2147c7b1482c2feb9f249c8e99f1a9
Kady Tran <kadytrn@gmail.com>
2025-11-18 21:32:48 -0800

    redid the footer component to include link

--- project-draft/src/components/Footer.jsx
+++ project-draft/src/components/Footer.jsx
@@ -1,11 +1,11 @@
+import { Link } from "react-router";
+
 export default function Footer() {
     return (
-        <footer>
-            <div className="footer-nav">
-                <a href="index.html">Home</a>
-                <a href="shop.html">Businesses</a>
-                <a href="proposal.html">About</a>
-            </div>
+        <footer className="footer">
+                <Link to="/">Home</Link>
+                <Link to="/customer">Customers</Link>
+                <Link to="/business">Businesses</Link>
             <p>&copy; 2025 ThriveTogether | Contact: info@thrivetogether.com</p>
         </footer>
     );
