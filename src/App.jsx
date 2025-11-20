feba287e1f5eda67e35e0178acb159997a939b7e
AnuujinChadraa <anuujinchadraa@gmail.com>
2025-11-20 14:05:56 -0800

    firebase connected

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import Intro from "./components/Intro";
 import CustomerPage from "./components/CustomerPage";
 import BusinessPage from "./components/BusinessPage";
 import './style.css';
+import { auth, db } from './firebase';
 
 /* https://stackoverflow.com/questions/57156433/navigating-to-different-views-screens-in-reactjs-onclick */
 function App() {
+
+  console.log("Firebase services loaded:", auth, db);
+
   return (
     <Router>
       <main>
