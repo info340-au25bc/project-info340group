2642b5a4c8b9cc4d307389825ed3dbbd9498abbd
hriesha popat <hpopat.uw@edu>
2025-12-01 12:10:53 -0800

    added click to logo so user can go back to home page

--- project-draft/src/components/Header.jsx
+++ project-draft/src/components/Header.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return (
         <header>
             <div className="header-container">
                 <div className="logo">
-                    <h1>ThriveTogether</h1>
+                    <Link to="/" className="logo-link">
+                        <h1>ThriveTogether</h1>
+                    </Link>
                 </div>
                 <div className="search-container">
                     <span className="search-icon">🔍</span>
