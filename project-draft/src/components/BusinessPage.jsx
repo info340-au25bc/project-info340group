dd05caefc6f5d5ffc5c39f48bf61801c1ab7663e
hriesha popat <hpopat.uw@edu>
2025-12-06 17:30:02 -0800

    fixed business page issue

--- project-draft/src/components/BusinessPage.jsx
+++ project-draft/src/components/BusinessPage.jsx
@@ -6,7 +6,7 @@ import Footer from './Footer';
 export default function BusinessPage() {
     return (
         <>
-            <Header currentUser={currentUser}/>
+            <Header />
             <Hero />
             <BusinessForm />
             <Footer />
